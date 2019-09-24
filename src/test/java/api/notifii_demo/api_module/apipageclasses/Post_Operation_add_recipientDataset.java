0782fc03566a03e3d0c117ae70741f9125bb325c
root <santhosh.annam@devrabbit.com>
2019-09-24 12:20:54 +0530

    dev

---
+++ src/test/java/api/notifii_demo/api_module/apipageclasses/Post_Operation_add_recipientDataset.java
@@ -0,0 +1,85 @@
+package api.notifii_demo.api_module.apipageclasses; 
+
+public class Post_Operation_add_recipientDataset{
+
+public static final int datasetLength = 1; 
+
+public static String projectName = "notifii_demo";
+
+ public static String primaryInfo  = "{\"module_description\":\"Notifii_API_Testing\\n\","
+ + "\"docker_files_directory_url\":\"https://smartqe.io:443/pdownload/DockerFiles\","
+ + "\"mobile_platform\":null,"
+ + "\"report_upload_url\":\"https://smartqe.io:443/UploadReportFile\","
+ + "\"repository_commit_message\":\"dev\","
+ + "\"browser_type\":null,"
+ + "\"client_timezone_id\":\"Asia/Calcutta\","
+ + "\"docker_files_directory\":\"/opt/tomcat9/webapps/pdownload/DockerFiles\","
+ + "\"project_url\":\"https://devrabbitdev.com/api/track4/\","
+ + "\"jenkins_username\":\"devrabbit\","
+ + "\"testcase_id\":302,"
+ + "\"project_name\":\"notifii_demo\","
+ + "\"testset_name\":null,"
+ + "\"is_web\":false,"
+ + "\"jenkins_password\":\"rabbit\","
+ + "\"testcase_overwrite\":true,"
+ + "\"docker_file_name\":\"DockerFile_7_SE_TC_302\","
+ + "\"project_id\":438,"
+ + "\"framework_path\":\"/home/devrabbit/SmartQE/SmartQE-Users/7/Selenium/notifii_demo\","
+ + "\"sub_module_id\":0,"
+ + "\"repository_url\":\"VKd24ScoUJiYt3dWUfip8BGY4MEiL+hQwPZlfa2nmfrDqulElDmAuhKW3TLCo795A1xC1klvgxuYRi2lz2aXs5eKPqNYYxHCMymul3y4lOCVOb+UxjRPfnJyZdGxfjsz\","
+ + "\"sub_module_description\":null,"
+ + "\"testcase_name\":\"Notifii_TC\","
+ + "\"executed_timestamp\":1569307851824,"
+ + "\"is_execute\":false,"
+ + "\"file_name\":null,"
+ + "\"aes_secret_key\":\"_SmartQE_SmartQE\","
+ + "\"testset_id\":0,"
+ + "\"jenkins_token\":\"114de69b453efd3524fce8756b1ed0e69c\","
+ + "\"is_generate\":false,"
+ + "\"jenkins_url\":\"http://192.168.1.141:8080/\","
+ + "\"project_description\":\"desc\","
+ + "\"module_id\":734,"
+ + "\"user_id\":7,"
+ + "\"bundle_id\":null,"
+ + "\"sub_module_name\":null,"
+ + "\"device_os_version\":null,"
+ + "\"execution_environment\":\"jenkins\","
+ + "\"module_name\":\"API_Module\","
+ + "\"executed_user_id\":4}";
+
+
+			/*--------Default Dataset-----------*/
+
+public static final String datasetHeader1 = "Notifii_TC-Default";
+
+public static final String datasetResources1 = "add-recipient.php";
+public static final int requestType1 = 2;
+public static final int bodyType1 = 3;
+
+ public static String urlParams1  = "[]";
+
+ public static String headers1  = "[]";
+
+ public static String body1  = "{\"raw_text\":\"{\\n    \\\"session_id\\\": \\\"11003937\\\","
+ + "\\n    \\\"authentication_token\\\": \\\"sgqkvwfo7rrisy7juf16fcqq8ctnb5\\\","
+ + "\\n    \\\"account_id\\\": \\\"52519\\\","
+ + "\\n    \\\"first_name\\\": \\\"Devrabbit\\\","
+ + "\\n    \\\"last_name\\\": \\\"Resident1\\\","
+ + "\\n    \\\"address1\\\": \\\"D1\\\","
+ + "\\n    \\\"email\\\": \\\"\\\","
+ + "\\n    \\\"cellphone\\\": \\\"\\\","
+ + "\\n    \\\"send_track_nonmarketing_email\\\": \\\"0\\\","
+ + "\\n    \\\"send_track_nonmarketing_text\\\": \\\"0\\\","
+ + "\\n    \\\"recipient_status\\\": \\\"1\\\","
+ + "\\n    \\\"recipient_type\\\": \\\"31\\\","
+ + "\\n    \\\"special_track_instructions_flag\\\": \\\"none\\\","
+ + "\\n    \\\"special_track_instructions\\\": \\\"\\\","
+ + "\\n    \\\"vacation_start_date\\\": \\\"\\\","
+ + "\\n    \\\"vacation_end_date\\\": \\\"\\\","
+ + "\\n    \\\"send_pkg_login_notification\\\": \\\"a\\\","
+ + "\\n    \\\"send_pkg_logout_notification\\\": \\\"a\\\"\\n}\","
+ + "\"raw_id\":1126,"
+ + "\"dataset_id\":1384,"
+ + "\"raw_type_id\":3}";
+
+}
